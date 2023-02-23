1355a3170f3abe2bb15536e17759c8169508932a
DoengesBA <110855912+DoengesBA@users.noreply.github.com>
2023-02-22 21:24:34 -0700

    controllers

---
+++ controllers/userController.js
@@ -0,0 +1,8 @@
+const User = require('../models/Users');
+
+module.exports = {
+    getUsers(req, res) {
+      User.find()
+        .then((users) => res.json(users))
+        .catch((err) => res.status(500).json(err));
+    },
